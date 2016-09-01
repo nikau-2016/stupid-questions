a60a1d2993f30abcb7bd94f4824d270593bbc978
Justin <justindado@yahoo.com>
2016-09-01 16:28:37 +1200

    Postgres server details added

--- knexfile.js
+++ knexfile.js
@@ -28,9 +28,11 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: 'ec2-184-73-202-229.compute-1.amazonaws.com',
+      port: '5432',
+      database: 'dat16vcqpa25mp',
+      user:     'eugprbskmqtovo',
+      password: 'process.env.DBPWD'
     },
     pool: {
       min: 2,
