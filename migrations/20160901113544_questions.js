d8ca5e9132828121e1f141ff2d1e3c3d8f03ae87
Sach <shsaci@yahoo.com.au>
2016-09-05 12:11:00 +1200

    Edit on last commit, changed both question and answer migration to accept timestamps

--- migrations/20160901113544_questions.js
+++ migrations/20160901113544_questions.js
@@ -4,7 +4,7 @@ exports.up = (knex, Promise) => {
     table.increments('id').primary()
     table.string('content')
     table.string('title')
-    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'))
+    table.timestamp('created').notNullable().defaultTo(knex.raw('now()'))
   })
 }
 
--- migrations/20160901113552_answers.js
+++ migrations/20160901113552_answers.js
@@ -3,7 +3,7 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('answers', (table) => {
       table.increments('id').primary()
       table.string('content')
-      table.string('created')
+      table.timestamp('created').notNullable().defaultTo(knex.raw('now()'))
       table.integer('question_id')
   })
 }
