319397d49655d7789ec9a9174ab569dfeceadda4
Sach <shsaci@yahoo.com.au>
2016-09-05 12:14:57 +1200

    final timestamp

--- migrations/20160901113552_answers.js
+++ migrations/20160901113552_answers.js
@@ -3,7 +3,7 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('answers', (table) => {
       table.increments('id').primary()
       table.string('content')
-      table.timestamp('created').notNullable().defaultTo(knex.raw('now()'))
+      table.timestamp('created').notNullable().defaultTo(knex.fn('now()'))
       table.integer('question_id')
   })
 }
