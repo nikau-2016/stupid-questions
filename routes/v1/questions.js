b18a483f68adf22b9d3910353733f34346fcdd3c
Rich Churcher <rich.churcher@gmail.com>
2016-09-06 16:40:23 +1200

    Fix formatting/uppercase

--- routes/v1/questions.js
+++ routes/v1/questions.js
@@ -1,37 +1,37 @@
 const express = require('express')
-const Router = express.Router()
-const db = require("../../lib/database")
+const router = express.Router()
+const db = require('../../lib/database')
 
-module.exports = Router
+module.exports = router
 
-Router.get('/', (req, res) => {
+router.get('/', (req, res) => {
   db.getAllQuestions()
     .then((data) => {
-      res.json({data:data})
+      res.json({data: data})
     })
-    .catch((error) => res.sendStatus(500))
+    .catch(() => res.sendStatus(500))
 })
 
-Router.post('/', (req, res) => {
+router.post('/', (req, res) => {
   const content = req.body.content
   const title = req.body.title
   db.addQuestion(content, title)
     .then((data) => {
       res.sendStatus(201)
     })
-    .catch((error) => res.sendStatus(500))
+    .catch(() => res.sendStatus(500))
 })
 
-Router.get('/:id/answers', (req, res) => {
+router.get('/:id/answers', (req, res) => {
   const id = req.params.id
   db.getAnswersById(id)
     .then((data) => {
-      res.json({data:data})
+      res.json({data: data})
     })
-    .catch((error) => res.sendStatus(500))
+    .catch(() => res.sendStatus(500))
 })
 
-Router.post('/:id/answers', (req, res) => {
+router.post('/:id/answers', (req, res) => {
   const content = req.body.content
   const id = req.params.id
   db.addAnswer(content, id)
